eb9283f268982f31d4b4d0094aea6d0ecd5f7483
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-09-16 18:32:46 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,12 +1,3 @@
 def find_min_value(array)
-  index = 0
-  min_value = 1
-
-  while index < array.length do
-    if min_value > array[index]
-      min_value = array[index]
-    end
-  index +=1
-  end
-  min_value
+  array.min
 end
